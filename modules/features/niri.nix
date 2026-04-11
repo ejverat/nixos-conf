120f1fd286c0fb9de45ce82942a93acdb649ed35
Emmanuel Vera <emm.vera@gmail.com>
2026-04-11 02:19:41 -0600

    Add additional packages and update configuration

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -1,14 +1,14 @@
 { self, inputs, ... }:
 {
 
-	flake.nixosModules.niri = { pkgs, lib, ... }: {
+	flake.nixosModules.niri = { config, pkgs, lib, ... }: {
 		programs.niri = {
 			enable = true;
 			package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
 		};
 	};
 
-	perSystem = { pkgs, lib, self', ... }: 
+	perSystem = { config, pkgs, lib, self', ... }: 
 	let
 		noctaliaCmd = lib.getExe self'.packages.myNoctalia;
 	in
@@ -16,9 +16,11 @@
 		packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
 			inherit pkgs;
 			settings = {
+
 				spawn-at-startup = [
 					noctaliaCmd
 				];
+
 				input.keyboard = {
 					xkb.layout = "us,latam";
 				};
@@ -26,10 +28,13 @@
 				layout.gaps = 4;
 
 				binds = {
+          "Mod+Shift+Slash".show-hotkey-overlay = (_: {});
 					"Mod+S".spawn-sh = "${noctaliaCmd} ipc call launcher toggle";
 					"Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
 					"Mod+Q".close-window = (_: {});
+          "Super+Ctrl+L".spawn-sh = "${noctaliaCmd} ipc call lockScreen lock";
 				};
+        xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
 			};
 		};
 	};
--- modules/hosts/chopper/configuration.nix
+++ modules/hosts/chopper/configuration.nix
@@ -54,11 +54,16 @@
 			pciutils
 			asusctl
 			file-roller
+      gcc
+      ripgrep
+      luarocks
 		];
-		
+
+    environment.variables.EDITOR = lib.getExe pkgs.neovim;
+
 		programs.xfconf.enable = true;
 		programs.thunar.enable = true;
-		programs.thunar.plugins = with pkgs.xfce; [
+		programs.thunar.plugins = with pkgs; [
 		  thunar-archive-plugin
 		  thunar-volman
 		];
